e63b1efd9de7cca0b466ed1b945f488569bff687
SumanthKumar <sumanthkumar.shettyc@mediakind.com>
2024-01-01 01:00:21 +0530

    Added WireMock tool demo Proj

---
+++ WireMockToolDemo/ConsoleApp1/TestHelloWorldSampleMock.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Routing;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WireMockDemo
+{
+    internal class TestHelloWorldSampleMock
+    {
+        private RestClient client;
+
+        public async Task TestHelloWorldMock()
+        {
+            // This creates the mock API response we defined earlier
+            new WireMock().CreateHelloWorldStub();
+
+            // Define the HTTP request to be sent
+            client = new RestClient("http://localhost:9876");
+
+            RestRequest request = new RestRequest("/hello-world", Method.Get);
+
+            // Send the HTTP request to the mock server
+            RestResponse response = await client.ExecuteAsync(request);
+
+            // Check that the response returned by the mock server contains the expected properties
+            if (response.StatusCode == HttpStatusCode.OK && response.ContentType.Equals("text/plain"))
+            {
+                Console.WriteLine("Verified");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+}
---
+++ WireMockToolDemo/ConsoleApp1/WireMock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace WireMockDemo
+{
+    public class WireMock
+    {
+        private WireMockServer server;
+        public void StartServer()
+        {
+            // This starts a new mock server instance listening at port 9876
+            server = WireMockServer.Start(9876);
+        }
+
+        public void CreateHelloWorldStub()
+        {
+            // This defines a mock API response that responds to an incoming HTTP GET
+            // to the `/hello-world` endpoint with a response with HTTP status code 200,
+            // a Content-Type header with value `text/plain` and a response body
+            // containing the text `Hello, world!`
+            server.Given(
+                Request.Create().WithPath("/hello-world").UsingGet()
+            )
+            .RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "text/plain")
+                    .WithBody("Hello, world!")
+            );
+        }
+
+        public void StopServer()
+        {
+            // This stops the mock server to clean up after ourselves
+            server.Stop();
+        }
+    }
+}
